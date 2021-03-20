95f2a0f730c1cc8cf8c60134d9cadf1147795702
Chloe Everson <chloeliz93@gmail.com>
2021-03-19 22:21:14 -0500

    update: required through addItem problem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,16 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = []
+function addItem( item ) {
+  if (item) {
+    basket.push(item);
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('added item? no item expect false' , addItem());
+console.log(`Basket is ${basket}`);
+console.log('added item? Adding bananas (expect true)', addItem('bananas'));
+console.log(`Basket is now ${basket}`);
