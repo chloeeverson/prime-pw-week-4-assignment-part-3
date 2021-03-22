4daaa79823b4bff6ff98c4d90642ca615fa95ab3
Chloe Everson <chloeliz93@gmail.com>
2021-03-21 19:30:00 -0500

    update: up to listItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -14,3 +14,36 @@ console.log('added item? no item expect false' , addItem());
 console.log(`Basket is ${basket}`);
 console.log('added item? Adding bananas (expect true)', addItem('bananas'));
 console.log(`Basket is now ${basket}`);
+console.log('added item? Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+// function listItems() {
+//   for (let i = 0; i<basket.length; i++ ){
+//   console.log(basket[i]);
+//   }
+// }
+
+function listItems() {
+  for (let i = 0; i<basket.length; i++ ){
+  return basket[i]
+  }
+}
+
+console.log('items banana and apples should be listed on seperate lines:' , listItems());
+
+// function listItems() {
+//   for (item of basket) {
+//     console.log(item);
+//   }
+// }
+// console.log(listItems());
+
+
+function empty(){
+  while (basket.length > 0) {
+    basket.pop();
+  }
+  return basket
+}
+
+console.log('basket should be empty:' , empty());
